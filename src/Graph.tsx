a47a33ce9f57a304dda5401d72a5ab7df381fc88
Ricki Angel <angelricki@outlook.com>
2024-09-01 03:12:28 +0100

    Edited the Grapg.tsx to resolve the componentDidUpdate method. Grapg works but doesn't show upper bound green line.

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Table } from '@finos/perspective';
+import {Table, TableData} from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import { DataManipulator } from './DataManipulator';
 import './Graph.css';
@@ -18,6 +18,8 @@ class Graph extends Component<IProps, {}> {
     return React.createElement('perspective-viewer');
   }
 
+
+  // @ts-ignore
   componentDidMount() {
     // Get element from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
@@ -29,7 +31,7 @@ class Graph extends Component<IProps, {}> {
       timestamp: 'date',
       upper_band: 'float',
       lower_bound: 'float',
-     trigger_alert: 'float',
+      trigger_alert: 'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -43,23 +45,28 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute('row-pivots', '["timestamp"]');
       elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-          price_abc: 'avg',
-          price_def: 'avg',
-          ratio: 'avg',
-          timestamp: 'distinct count',
-          upper_bound: 'avg',
-          lower_bound: 'avg',
-          trigger_alert: 'avg',
-    }));
+        price_abc: 'avg',
+        price_def: 'avg',
+        ratio: 'avg',
+        timestamp: 'distinct count',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'avg',
+      }));
+    }
+
+
   }
+
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
+      this.table.update([
         DataManipulator.generateRow(this.props.data),
-      );
-    }
-  }
+          ] as unknown as TableData);
+
+    }}
+
 }
 
 export default Graph;
